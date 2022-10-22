708608089815f6874ad0f863734b10c02646819b
thimo <thimo@TT_LAPTOP>
2022-10-22 18:22:01 +0200

    added functionality for OUT/BRIDGE

--- CircuitGrid/src/App/Updatefunctions.cpp
+++ CircuitGrid/src/App/Updatefunctions.cpp
@@ -35,52 +35,6 @@ bool Simulationscreen::update_wire(uint32_t& i) {
 
 	//up
 	if (this_board[i_up] < BRIDGE) {
-		if (this_board[i_up+1] > max_surrounding_electricity) {
-			max_surrounding_electricity = this_board[i_up + 1];
-		}
-	}
-	//left
-	if (this_board[i_left] < BRIDGE) {
-		if (this_board[i_left+1] > max_surrounding_electricity) {
-			max_surrounding_electricity = this_board[i_left + 1];
-		}
-	}
-	//right
-	if (this_board[i_right] < BRIDGE) {
-		if (this_board[i_right+1] > max_surrounding_electricity) {
-			max_surrounding_electricity = this_board[i_right + 1];
-		}
-	}
-	//down
-	if (this_board[i_down] < BRIDGE) {
-		if (this_board[i_down+1] > max_surrounding_electricity) {
-			max_surrounding_electricity = this_board[i_down + 1];
-		}
-	}
-
-
-	if (this_board[this_i+1] > max_surrounding_electricity) {
-		next_board[this_i + 1] = 0;
-		return true;
-	}
-	else {
-		next_board[this_i + 1] = max_surrounding_electricity - 1;
-	}
-
-	return next_board[this_i +1] != this_board[this_i + 1];
-}
-
-bool Simulationscreen::update_out(uint32_t& i) {
-	uint32_t this_i = i * 4;
-	uint32_t i_up = (i - board_width) * 4;
-	uint32_t i_down = (i + board_width) * 4;
-	uint32_t i_left = (i - 1) * 4;
-	uint32_t i_right = (i + 1) * 4;
-
-	uint32_t max_surrounding_electricity = 0;
-
-	//up
-	if (this_board[i_up] < BRIDGE) {
 		if (this_board[i_up + 1] > max_surrounding_electricity) {
 			max_surrounding_electricity = this_board[i_up + 1];
 		}
@@ -104,7 +58,53 @@ bool Simulationscreen::update_out(uint32_t& i) {
 		}
 	}
 
-	return false;
+
+	if (this_board[this_i + 1] > max_surrounding_electricity) {
+		next_board[this_i + 1] = 0;
+		return true;
+	}
+	else {
+		next_board[this_i + 1] = max_surrounding_electricity - 1;
+	}
+
+	return *(uint32_t*)&this_board[this_i] != *(uint32_t*)&next_board[this_i];
+}
+
+bool Simulationscreen::update_out(uint32_t& i) {
+	uint32_t this_i = i * 4;
+	uint32_t i_up = (i - board_width) * 4;
+	uint32_t i_down = (i + board_width) * 4;
+	uint32_t i_left = (i - 1) * 4;
+	uint32_t i_right = (i + 1) * 4;
+
+	uint32_t max_surrounding_electricity = 0;
+
+	//up
+	if (this_board[i_up] >= BUTTON) {
+		if (this_board[i_up + 1] > 0) {
+			next_board[this_i + 1] = this_board[i_up + 1];
+		}
+	}
+	//left
+	if (this_board[i_left] >= BUTTON) {
+		if (this_board[i_left + 1] > 0) {
+			next_board[this_i + 1] = this_board[i_left + 1];
+		}
+	}
+	//right
+	if (this_board[i_right] >= BUTTON) {
+		if (this_board[i_right + 1] > 0) {
+			next_board[this_i + 1] = this_board[i_right + 1];
+		}
+	}
+	//down
+	if (this_board[i_down] >= BUTTON) {
+		if (this_board[i_down + 1] > 0) {
+			next_board[this_i + 1] = this_board[i_down + 1];
+		}
+	}
+
+	return *(uint32_t*)&this_board[this_i] != *(uint32_t*)&next_board[this_i];
 }
 
 bool Simulationscreen::update_battery(uint32_t& i) {
@@ -112,7 +112,58 @@ bool Simulationscreen::update_battery(uint32_t& i) {
 }
 
 bool Simulationscreen::update_bridge(uint32_t& i) {
-	return false;
+	uint32_t this_i = i * 4;
+	uint32_t i_up = (i - board_width) * 4;
+	uint32_t i_down = (i + board_width) * 4;
+	uint32_t i_left = (i - 1) * 4;
+	uint32_t i_right = (i + 1) * 4;
+
+	uint32_t max_surrounding_electricity_horizontal = 0;//---
+	uint32_t max_surrounding_electricity_vertical = 0;//|||
+
+	//left
+	if (this_board[i_left] < BRIDGE) {
+		if (this_board[i_left + 1] > max_surrounding_electricity_horizontal) {
+			max_surrounding_electricity_horizontal = this_board[i_left + 1];
+		}
+	}
+	//right
+	if (this_board[i_right] < BRIDGE) {
+		if (this_board[i_right + 1] > max_surrounding_electricity_horizontal) {
+			max_surrounding_electricity_horizontal = this_board[i_right + 1];
+		}
+	}
+
+	//up
+	if (this_board[i_up] < BRIDGE) {
+		if (this_board[i_up + 1] > max_surrounding_electricity_vertical) {
+			max_surrounding_electricity_vertical = this_board[i_up + 1];
+		}
+	}
+	//down
+	if (this_board[i_down] < BRIDGE) {
+		if (this_board[i_down + 1] > max_surrounding_electricity_vertical) {
+			max_surrounding_electricity_vertical = this_board[i_down + 1];
+		}
+	}
+
+	//horizontal check
+	if (this_board[this_i + 1] > max_surrounding_electricity_horizontal) {
+		next_board[this_i + 1] = 0;
+	}
+	else {
+		next_board[this_i + 1] = max_surrounding_electricity_horizontal - 1;
+	}
+
+	//vertical check
+	if (this_board[this_i + 2] > max_surrounding_electricity_vertical) {
+		next_board[this_i + 2] = 0;
+	}
+	else {
+		next_board[this_i + 2] = max_surrounding_electricity_vertical - 1;
+	}
+
+	return *(uint32_t*)&this_board[this_i] != *(uint32_t*)&next_board[this_i];
 }
 
 bool Simulationscreen::update_lamp(uint32_t& i) {
